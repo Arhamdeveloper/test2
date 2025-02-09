984af3b762b79f27cd6cb8d9a130ef710e402cfe
Arham Developer <marham.zain2204@gmail.com>
2025-02-09 23:51:31 +0500

    Add files via upload
    j

---
+++ script.js
@@ -0,0 +1,10 @@
+let object = {
+    Name : "Arham" ,
+    "Father name" : "Zainuddin",
+    Education : "Matric" ,
+    job :   "Computer Operator"
+}
+console.log(object["job"])
+object.job= "Photocopier" ; 
+console.log(object["job"])
+console.log(object)
